804e831ffc5de22f54be8b4a9eaa2f27465abb1d
mukkasaiteja <msaiteja2669@gmail.com>
2023-05-30 12:29:15 +0530

    homr

---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './Main.css'
+
+function Main() {
+  return (
+    <div className='main'>  
+    <h1>Home</h1>
+        {/* <img src="https://moewalls.com/wp-content/uploads/2023/05/snow-mountain-house-thumb-728x410.jpg" alt="" /> */}
+    </div>
+  )
+}
+
+export default Main
---
+++ src/components/Nav.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Nav.css'
+
+function Nav() {
+  return (
+    <div className='nav'>
+        <section className='title'>Logo</section>
+        <section className='nav-items'>
+            <ul>
+                <li>HOME</li>
+                <li>ABOUT</li>
+                <li>MENU</li>
+                <li>ORDER</li>
+                <li>CONTACT</li>
+            </ul>
+        </section>
+
+
+
+
+
+
+    </div>
+  )
+}
+
+export default Nav
